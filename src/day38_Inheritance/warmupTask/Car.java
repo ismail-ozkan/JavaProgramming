c54e2e37a064a6a6f19342709dbeb9ef9dc1f9e4
ismail-ozkan <ismailozkanlaw@gmail.com>
2022-12-13 13:34:21 +0300

    day38 Inheritance Car Class

---
+++ src/day38_Inheritance/warmupTask/Car.java
@@ -0,0 +1,35 @@
+package day38_Inheritance.warmupTask;
+
+public class Car {
+
+    public String brand, model, color;
+    public int year, miles;
+    public double price;
+
+    public Car(String brand, String model, String color, int year, int miles, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.miles = miles;
+        this.price = price;
+    }
+
+    public void start(){
+        System.out.println(brand + " " + model + " is starting");
+    }
+    public void drive(){
+        System.out.println(brand + " " + model + " is driving");
+    }
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", miles=" + miles +
+                ", price= $" + price +
+                '}';
+    }
+}
