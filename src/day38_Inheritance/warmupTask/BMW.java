abd323ed17657969b27e521ccaaf2a3e6fe916ea
ismail-ozkan <ismailozkanlaw@gmail.com>
2022-12-13 13:55:29 +0300

    BMW and Tesla Class

---
+++ src/day38_Inheritance/warmupTask/BMW.java
@@ -0,0 +1,15 @@
+package day38_Inheritance.warmupTask;
+
+public class BMW extends Car{
+
+    public BMW(String model, String color, int year, int miles, double price) {
+        super("BMW", model, color, year, miles, price);
+    }
+
+    public void breaksDown(){
+
+    }
+    public void racing() {
+
+    }
+}
---
+++ src/day38_Inheritance/warmupTask/Tesla.java
@@ -0,0 +1,12 @@
+package day38_Inheritance.warmupTask;
+
+public class Tesla extends Car{
+    public Tesla(String model, String color, int year, int miles, double price) {
+        super("Tesla", model, color, year, miles, price);
+    }
+
+    public void autoPilot(){
+
+    }
+
+}
