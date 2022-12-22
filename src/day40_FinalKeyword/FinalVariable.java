3c953315a4229aa4c0792edea652f0923eaa77b0
ismail-ozkan <ismailozkanlaw@gmail.com>
2022-12-22 15:11:38 +0300

    day40 completed

---
+++ src/day40_FinalKeyword/FinalVariable.java
@@ -0,0 +1,36 @@
+package day40_FinalKeyword;
+
+public class FinalVariable {
+
+    final String birthDay;
+    final static String name;
+
+    public FinalVariable(String birthDay){
+        this.birthDay = birthDay;
+    }
+
+
+    static {
+        name = "İsmail";
+    }
+
+    public static void main(String[] args) {
+
+        final double PI = 3.14;
+
+
+
+        String name = "Java";
+
+
+    //    FinalVariable.name = "Pyhton";
+
+
+
+
+
+    }
+
+
+
+}
